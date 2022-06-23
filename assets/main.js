85db6fee9de8fb08c142e227119cc2ef7f6ab8bf
Cindy Kerubo <cindykerubo@gmail.com>
2022-06-23 16:38:08 +0000

    plus 20

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,7 @@
 const button5 = document.querySelector("#plusFive")
 const button10 = document.querySelector("#plusTen")
 const button15 = document.querySelector("#plusFifteen")
+const button20 = document.querySelector("#plusTwenty")
 const indicator = document.querySelector("#number-indicator")
 const disp = document.querySelector("#display-heading")
 
@@ -17,4 +18,9 @@ button10.addEventListener('click', () => {
 button15.addEventListener('click', () => {
   disp.textContent = "added 15"
   indicator.textContent = (parseInt(indicator.textContent) + 15).toString()
+})
+
+button20.addEventListener('click', () => {
+  disp.textContent = "added 20"
+  indicator.textContent = (parseInt(indicator.textContent) + 20).toString()
 })
